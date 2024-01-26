f1bed71afb81a69c2e0c6cbab0b82723e6681395
Fabbye3737 <fabian.besca01@e-uvt.ro>
2024-01-26 15:22:03 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -60,12 +60,18 @@ CREATE TABLE IF NOT EXISTS gym_members(
 );
 CREATE TABLE IF NOT EXISTS equipment(
     id serial not null,
-    gym_id integer not null,
     name varchar(100) not null,
     quantity integer not null,
     condition char(20) not null,
+    primary key(id)
+);
+CREATE TABLE IF NOT EXISTS gym_equipment(
+    id serial not null,
+    gym_id integer not null,
+    equipment_id integer not null,
     primary key(id),
-    foreign key(gym_id) references gym(id)
+    foreign key(gym_id) references gym(id),
+	foreign key(equipment_id) references equipment(id)
 );
 CREATE TABLE IF NOT EXISTS workout_plans(
     id serial not null,
@@ -84,9 +90,9 @@ CREATE TABLE IF NOT EXISTS members_classes(
 );
 CREATE TABLE IF NOT EXISTS feedback(
     id serial not null,
-    member_id integer not null,
-    trainer_id integer not null,
-    classes_id integer not null,
+    member_id integer not null UNIQUE,
+    trainer_id integer not null UNIQUE,
+    classes_id integer not null UNIQUE,
     rating smallint not null,
     feedback_date date not null,
     primary key(id),
@@ -106,12 +112,18 @@ CREATE TABLE IF NOT EXISTS subscriptions(
     foreign key(gym_id) references gym(id),
     foreign key(member_id) references members(id)
 );
+CREATE TABLE IF NOT EXISTS facilities(
+    id serial not null,
+    facilities_type varchar(100) not null,
+    primary key(id)
+);
 CREATE TABLE IF NOT EXISTS gym_facilities(
     id serial not null,
     gym_id integer not null,
-    facility_name varchar(100) not null,
+    facility_id integer not null,
     primary key(id),
-    foreign key(gym_id) references gym(id)
+    foreign key(gym_id) references gym(id),
+    foreign key(facility_id) references facilities(id)
 );
 CREATE TABLE IF NOT EXISTS members_goals(
     id serial not null,
